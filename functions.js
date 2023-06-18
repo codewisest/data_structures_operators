cb99f1f70767dac68148c8a794ab39f9fb0d98f2
Chijioke <wisestcodes@gmail.com>
2023-06-18 16:05:17 -0400

    call and apply

--- functions.js
+++ functions.js
@@ -1,74 +1,108 @@
 'use strict';
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (flightNum, numPassenger = 1, price = 1) {
-  const booking = {
-    flightNum,
-    numPassenger,
-    price,
-  };
-  console.log(booking);
-  //   bookings.push(booking);
+// const createBooking = function (flightNum, numPassenger = 1, price = 1) {
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price,
+//   };
+//   console.log(booking);
+//   //   bookings.push(booking);
+// };
+
+// const flight = 'LH234';
+// let chiji = {
+//   name: 'Chijioke Nwagwu',
+//   passport: 82743779384,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 82743779384) {
+//     // alert('Check in');
+//   } else {
+//     // alert('Wrong passport');
+//   }
+// };
+
+// console.log(flight);
+// console.log(chiji);
+
+// checkIn(flight, chiji);
+// console.log(flight);
+// console.log(chiji);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+
+// newPassport(chiji);
+
+// checkIn(flight, chiji);
+
+// const oneWord = function (str) {
+//   return str.replace(' ', '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// const transformer = function (str, fn) {
+//   //   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
+
+// // return function
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('hey');
+// greeterHey('Chiji');
+// greet('hey')('Gloria');
+
+// CALL AND APPLY
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
 };
 
-const flight = 'LH234';
-let chiji = {
-  name: 'Chijioke Nwagwu',
-  passport: 82743779384,
+lufthansa.book(239, 'Chijioke Nwagwu');
+lufthansa.book(436, 'Nathan Wisest');
+
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+const book = lufthansa.book;
 
-  if (passenger.passport === 82743779384) {
-    // alert('Check in');
-  } else {
-    // alert('Wrong passport');
-  }
-};
+book.call(eurowings, 23, 'Vincent Gloria');
+console.log(eurowings);
 
-console.log(flight);
-console.log(chiji);
+book.call(lufthansa, 40, 'Chizoba Ugwu');
+const flightData = [583, 'Vincent Ugwu'];
 
-checkIn(flight, chiji);
-console.log(flight);
-console.log(chiji);
-
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
-};
-
-newPassport(chiji);
-
-checkIn(flight, chiji);
-
-const oneWord = function (str) {
-  return str.replace(' ', '').toLowerCase();
-};
-
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
-
-const transformer = function (str, fn) {
-  //   console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-
-  console.log(`Transformed by: ${fn.name}`);
-};
-
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
-
-// return function
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
-
-const greeterHey = greet('hey');
-greeterHey('Chiji');
-greet('hey')('Gloria');
+book.apply(eurowings, flightData);
